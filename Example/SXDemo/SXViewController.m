968bda1c3e6b3b78c8c18857e3ef77dda84f3a4f
高鹏 <97282073@qq.com>
2018-03-18 11:21:48 +0800

    init

--- Example/SXDemo/SXViewController.m
+++ Example/SXDemo/SXViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "SXViewController.h"
-
+#import "UncaughtExceptionHandler.h"
 @interface SXViewController ()
 
 @end
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    InstallUncaughtExceptionHandler();
 }
 
 - (void)didReceiveMemoryWarning
